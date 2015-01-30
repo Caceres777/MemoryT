ed81e230a6b07cb15935bbc865eac97afbf998b8
OwenCal <owencalzadillagough@gmail.com>
2015-01-30 17:04:35 +0100

    commit 30/01/2015 Styles and colors

--- app/src/main/java/com/example/owen/pruebasliderfragment/AppMemoryt.java
+++ app/src/main/java/com/example/owen/pruebasliderfragment/AppMemoryt.java
@@ -6,8 +6,6 @@ package com.example.owen.pruebasliderfragment;
 
 import android.app.Application;
 import com.parse.Parse;
-import com.parse.ParseACL;
-import com.parse.ParseUser;
 
 public class AppMemoryt extends Application{
 
