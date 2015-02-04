8a1845d794ee72cd1cb9f6eb8c59de7419d28c10
OwenCal <owencalzadillagough@gmail.com>
2015-02-04 18:51:00 +0100

    2/2/2015 preferences (finished cambios)

--- app/src/main/java/com/example/owen/pruebasliderfragment/Settings_frag.java
+++ app/src/main/java/com/example/owen/pruebasliderfragment/Settings_frag.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -36,15 +37,17 @@ public class Settings_frag extends Fragment {
         super.onCreate(savedInstanceState);
     }
 
-
-
-
+    @Override
+    public void onResume() {
+        super.onResume();
+        cargarConfiguracion();
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_settings_frag, container, false);
-        prefs= getActivity().getSharedPreferences("MemorytPrefs", Context.MODE_PRIVATE);
+        prefs= PreferenceManager.getDefaultSharedPreferences(getActivity());
         editor=prefs.edit();
 
 
@@ -53,36 +56,36 @@ public class Settings_frag extends Fragment {
         switchNotificaciones=(Switch) v.findViewById(R.id.switch3);
         switchRecordatorios=(Switch) v.findViewById(R.id.switch4);
         switchSonidoTactil=(Switch) v.findViewById(R.id.switch5);
-        cargarConfiguracion();
+        //cargarConfiguracion();
 
         switchSonido.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                editor.putBoolean("Sonido", switchSonido.isChecked()).apply();
+                editor.putBoolean("Sonido", switchSonido.isChecked()).commit();
             }
         });
         switchVibracion.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                editor.putBoolean("Vibracion", switchVibracion.isChecked()).apply();
+                editor.putBoolean("Vibracion", switchVibracion.isChecked()).commit();
             }
         });
         switchNotificaciones.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                editor.putBoolean("Notificaciones", switchNotificaciones.isChecked()).apply();
+                editor.putBoolean("Notificaciones", switchNotificaciones.isChecked()).commit();
             }
         });
         switchRecordatorios.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                editor.putBoolean("Recordatorios", switchRecordatorios.isChecked()).apply();
+                editor.putBoolean("Recordatorios", switchRecordatorios.isChecked()).commit();
             }
         });
         switchSonidoTactil.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                editor.putBoolean("SonidoTactil", switchSonidoTactil.isChecked()).apply();
+                editor.putBoolean("SonidoTactil", switchSonidoTactil.isChecked()).commit();
             }
         });
         return v;
